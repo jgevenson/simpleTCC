3c8259a84f1c0c4806b3e05d15064ecbb855e9f1
Jake <jgevenson@gmail.com>
2020-03-12 12:27:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,752 @@
+//code here
+//
+let deaconLodgeHoles = [
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 1,
+		black: 444,
+		blue: 415,
+		white: 399,
+		holePar: 4,
+		holeHandicap: 3,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 2,
+		black: 540,
+		blue: 520,
+		white: 462,
+		holePar: 5,
+		holeHandicap: 5,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 3,
+		black: 188,
+		blue: 164,
+		white: 144,
+		holePar: 3,
+		holeHandicap: 13,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 4,
+		black: 419,
+		blue: 399,
+		white: 354,
+		holePar: 4,
+		holeHandicap: 9,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 5,
+		black: 433,
+		blue: 415,
+		white: 400,
+		holePar: 4,
+		holeHandicap: 7,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 6,
+		black: 115,
+		blue: 108,
+		white: 91,
+		holePar: 3,
+		holeHandicap: 17,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 7,
+		black: 432,
+		blue: 412,
+		white: 385,
+		holePar: 4,
+		holeHandicap: 1,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 8,
+		black: 408,
+		blue: 374,
+		white: 326,
+		holePar: 4,
+		holeHandicap: 15,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 9,
+		black: 543,
+		blue: 533,
+		white: 480,
+		holePar: 5,
+		holeHandicap: 11,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 10,
+		black: 373,
+		blue: 357,
+		white: 329,
+		holePar: 4,
+		holeHandicap: 14,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 11,
+		black: 196,
+		blue: 183,
+		white: 176,
+		holePar: 3,
+		holeHandicap: 12,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 12,
+		black: 340,
+		blue: 320,
+		white: 295,
+		holePar: 4,
+		holeHandicap: 18,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 13,
+		black: 450,
+		blue: 422,
+		white: 380,
+		holePar: 4,
+		holeHandicap: 4,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 14,
+		black: 534,
+		blue: 512,
+		white: 475,
+		holePar: 5,
+		holeHandicap: 8,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 15,
+		black: 452,
+		blue: 427,
+		white: 413,
+		holePar: 4,
+		holeHandicap: 2,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 16,
+		black: 375,
+		blue: 362,
+		white: 343,
+		holePar: 4,
+		holeHandicap: 16,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 17,
+		black: 200,
+		blue: 174,
+		white: 144,
+		holePar: 3,
+		holeHandicap: 6,
+	},
+	{
+		courseString: 'Deacons Lodge',
+		courseID: 3,
+		hole: 18,
+		black: 535,
+		blue: 525,
+		white: 495,
+		holePar: 5,
+		holeHandicap: 10,
+	},
+];
+let traditionalHoles = [
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 1,
+		black: null,
+		blue: null,
+		white: 307,
+		holePar: 4,
+		holeHandicap: 9
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 2,
+		black: null,
+		blue: null,
+		white: 145,
+		holePar: 3,
+		holeHandicap: 15
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 3,
+		black: null,
+		blue: null,
+		white: 241,
+		holePar: 4,
+		holeHandicap: 17
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 4,
+		black: null,
+		blue: null,
+		white: 275,
+		holePar: 4,
+		holeHandicap: 13
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 5,
+		black: null,
+		blue: null,
+		white: 367,
+		holePar: 4,
+		holeHandicap: 1
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 6,
+		black: null,
+		blue: null,
+		white: 156,
+		holePar: 3,
+		holeHandicap: 11
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 7,
+		black: null,
+		blue: null,
+		white: 354,
+		holePar: 4,
+		holeHandicap: 3
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 8,
+		black: null,
+		blue: null,
+		white: 318,
+		holePar: 4,
+		holeHandicap: 7
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 9,
+		black: null,
+		blue: null,
+		white: 370,
+		holePar: 4,
+		holeHandicap: 5
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 10,
+		black: null,
+		blue: null,
+		white: 346,
+		holePar: 4,
+		holeHandicap: 12
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 11,
+		black: null,
+		blue: null,
+		white: 182,
+		holePar: 3,
+		holeHandicap: 18
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 12,
+		black: null,
+		blue: null,
+		white: 305,
+		holePar: 4,
+		holeHandicap: 10
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 13,
+		black: null,
+		blue: null,
+		white: 171,
+		holePar: 3,
+		holeHandicap: 16
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 14,
+		black: null,
+		blue: null,
+		white: 292,
+		holePar: 4,
+		holeHandicap: 14
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 15,
+		black: null,
+		blue: null,
+		white: 330,
+		holePar: 4,
+		holeHandicap: 4
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 16,
+		black: null,
+		blue: null,
+		white: 318,
+		holePar: 4,
+		holeHandicap: 2
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 17,
+		black: null,
+		blue: null,
+		white: 350,
+		holePar: 4,
+		holeHandicap: 8
+	},
+	{
+		courseString: "Traditional",
+		courseID: 2,
+		hole: 18,
+		black: null,
+		blue: null,
+		white: 365,
+		holePar: 4,
+		holeHandicap: 6
+	}
+]
+let whitebirtchHoles = [
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 1,
+		black: 341,
+		blue: 322,
+		white: 309,
+		holePar: 4,
+		holeHandicap: 13
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 2,
+		black: 300,
+		blue: 269,
+		white: 244,
+		holePar: 4,
+		holeHandicap: 17
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 3,
+		black: 442,
+		blue: 423,
+		white: 400,
+		holePar: 4,
+		holeHandicap: 3
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 4,
+		black: 520,
+		blue: 510,
+		white: 490,
+		holePar: 5,
+		holeHandicap: 7
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 5,
+		black: 176,
+		blue: 168,
+		white: 147,
+		holePar: 3,
+		holeHandicap: 15
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 6,
+		black: 380,
+		blue: 349,
+		white: 329,
+		holePar: 4,
+		holeHandicap: 5
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 7,
+		black: 536,
+		blue: 483,
+		white: 457,
+		holePar: 5,
+		holeHandicap: 1
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 8,
+		black: 314,
+		blue: 308,
+		white: 293,
+		holePar: 4,
+		holeHandicap: 11
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 9,
+		black: 217,
+		blue: 207,
+		white: 193,
+		holePar: 3,
+		holeHandicap: 9
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 10,
+		black: 406,
+		blue: 388,
+		white: 370,
+		holePar: 4,
+		holeHandicap: 4
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 11,
+		black: 479,
+		blue: 433,
+		white: 400,
+		holePar: 4,
+		holeHandicap: 2
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 12,
+		black: 190,
+		blue: 185,
+		white: 144,
+		holePar: 3,
+		holeHandicap: 18
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 13,
+		black: 373,
+		blue: 363,
+		white: 353,
+		holePar: 4,
+		holeHandicap: 14
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 14,
+		black: 374,
+		blue: 353,
+		white: 326,
+		holePar: 4,
+		holeHandicap: 10
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 15,
+		black: 502,
+		blue: 461,
+		white: 429,
+		holePar: 5,
+		holeHandicap: 6
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 16,
+		black: 362,
+		blue: 354,
+		white: 324,
+		holePar: 4,
+		holeHandicap: 12
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 17,
+		black: 205,
+		blue: 193,
+		white: 144,
+		holePar: 3,
+		holeHandicap: 16
+	},
+	{
+		courseString: "Whitebirtch",
+		courseID: 1,
+		hole: 18,
+		black: 563,
+		blue: 523,
+		white: 464,
+		holePar: 5,
+		holeHandicap: 8
+	},
+];
+
+
+const courseSelect = document.getElementById('course-select'),
+	wholeForm = document.getElementById('whole-form'),
+	parDisplay = document.getElementById('par-display'),
+	holeSelect = document.getElementById('hole-select'),
+	courseHoleSubmit = document.getElementById('course-hole-select'),
+	result = document.getElementById('result'),
+	calculateHoleButton = document.getElementById('submit'),
+	team1ResultsArea = document.getElementById('team1-scores'),
+	team2ResultsArea = document.getElementById('team2-scores'),
+	team1Area = document.getElementById('team1-area'),
+	team2Area = document.getElementById('team2-area'),
+	t1p1upButton = document.getElementById('t1p1-up1'),
+	t1p1downButton = document.getElementById('t1p1-down1'),
+	t1p2upButton = document.getElementById('t1p2-up1'),
+	t1p2downButton = document.getElementById('t1p2-down1'),
+	t2p1upButton = document.getElementById('t2p1-up1'),
+	t2p1downButton = document.getElementById('t2p1-down1'),
+	t2p2upButton = document.getElementById('t2p2-up1'),
+	t2p2downButton = document.getElementById('t2p2-down1'),
+	resetButton = document.getElementById('reset-button');
+let team1Winner = true,
+	tied = false,
+	team1UnderPar = false,
+	team2UnderPar = false,
+	formFinished = false,
+	par = getPar();
+team1ResultsArea.style.display = 'none';
+team2ResultsArea.style.display = 'none';
+resetButton.style.display = 'none';
+parDisplay.style.display = 'none';
+
+courseHoleSubmit.addEventListener('click', getPar);
+
+function getPar() {
+	let h = parseInt(holeSelect.value) - 1;
+	let c = parseInt(courseSelect.value);
+	let p = (function(par) {
+		switch (c) {
+		case 1:
+			return whitebirtchHoles[h].holePar;
+			break;
+		case 2:
+			return traditionalHoles[h].holePar;
+			break;
+		case 3:
+			return deaconLodgeHoles[h].holePar;
+			break;
+	};});
+	document.getElementById('t1p1').defaultValue = p;
+	document.getElementById('t1p2').defaultValue = p;
+	document.getElementById('t2p1').defaultValue = p;
+	document.getElementById('t2p2').defaultValue = p;
+	parDisplay.style.display = 'block';
+	parDisplay.innerHTML = p;
+	return p;
+}
+
+
+//Up and Down Buttons
+//t1p1
+t1p1upButton.addEventListener('click', function () {
+	x = parseInt(document.getElementById('t1p1').value);
+	x++;
+	document.getElementById('t1p1').value = x;
+});
+t1p1downButton.addEventListener('click', function () {
+	x = parseInt(document.getElementById('t1p1').value);
+	x--;
+	document.getElementById('t1p1').value = x;
+});
+//t1p2
+t1p2upButton.addEventListener('click', function () {
+	x = parseInt(document.getElementById('t1p2').value);
+	x++;
+	document.getElementById('t1p2').value = x;
+});
+t1p2downButton.addEventListener('click', function () {
+	x = parseInt(document.getElementById('t1p2').value);
+	x--;
+	document.getElementById('t1p2').value = x;
+});
+//t2p1
+t2p1upButton.addEventListener('click', function () {
+	x = parseInt(document.getElementById('t2p1').value);
+	x++;
+	document.getElementById('t2p1').value = x;
+});
+t2p1downButton.addEventListener('click', function () {
+	x = parseInt(document.getElementById('t2p1').value);
+	x--;
+	document.getElementById('t2p1').value = x;
+});
+//t2p2
+t2p2upButton.addEventListener('click', function () {
+	x = parseInt(document.getElementById('t2p2').value);
+	x++;
+	document.getElementById('t2p2').value = x;
+});
+t2p2downButton.addEventListener('click', function () {
+	x = parseInt(document.getElementById('t2p2').value);
+	x--;
+	document.getElementById('t2p2').value = x;
+});
+
+//********ALL ACTIONS TAKEN WHEN THE SUBMIT BUTTON IS PRESSED*********
+calculateHoleButton.addEventListener('click', function () {
+	let par = getPar(),
+		t1p1 = parseInt(document.getElementById('t1p1').value),
+		t1p2 = parseInt(document.getElementById('t1p2').value),
+		t2p1 = parseInt(document.getElementById('t2p1').value),
+		t2p2 = parseInt(document.getElementById('t2p2').value),
+		team1Scores = [t1p1, t1p2],
+		team2Scores = [t2p1, t2p2];
+
+	//Making the Scores appear in the correct order
+	if (t2p1 < par || t2p2 < par) {
+		team1Scores.sort(function (a, b) {
+			return b - a;
+		});
+		team2UnderPar = true;
+	} else {
+		team1Scores.sort(function (a, b) {
+			return a - b;
+		});
+	}
+	if (t1p1 < par || t1p2 < par) {
+		team2Scores.sort(function (a, b) {
+			return b - a;
+		});
+		team1UnderPar = true;
+	} else {
+		team2Scores.sort(function (a, b) {
+			return a - b;
+		});
+	}
+	//Getting the Vegas Scores
+	let team1VegasScore = parseInt(
+		team1Scores[0].toString() + team1Scores[1].toString()
+	);
+	let team2VegasScore = parseInt(
+		team2Scores[0].toString() + team2Scores[1].toString()
+	);
+	//Determining the Winning Team
+	if (team1VegasScore < team2VegasScore) {
+		team1Winner = true;
+		tied = false;
+	} else if (team1VegasScore > team2VegasScore) {
+		team1Winner = false;
+		tied = false;
+	} else {
+		team1Winner = false;
+		tied = true;
+	}
+
+	//Finding the Scores and displaying scores
+	if (team1Winner) {
+		let team2AddToCard = team2VegasScore - team1VegasScore,
+			team1AddToCard = 0;
+		team1Area.style.backgroundColor = 'rgba(0, 128, 0, 0.3)';
+		team2Area.style.backgroundColor = 'rgba(255, 0, 0, 0.3)';
+		team2ResultsArea.style.display = 'block';
+		team1ResultsArea.style.display = 'none';
+		team2ResultsArea.innerHTML = 'Team 2 add ' + team2AddToCard + ' points';
+	} else if (!team1Winner && !tied) {
+		let team1AddToCard = team1VegasScore - team2VegasScore,
+			team2AddToCard = 0;
+		team2Area.style.backgroundColor = 'rgba(0, 128, 0, 0.3)';
+		team1Area.style.backgroundColor = 'rgba(255, 0, 0, 0.3)';
+		team1ResultsArea.style.display = 'block';
+		team2ResultsArea.style.display = 'none';
+		team1ResultsArea.innerHTML = 'Team 1 add ' + team1AddToCard + ' points';
+	} else {
+		team1AddToCard = 0;
+		team2AddToCard = 0;
+		team2Area.style.backgroundColor = 'rgb(255, 255, 255)';
+		team1Area.style.backgroundColor = 'rgb(255, 255, 255)';
+		team1ResultsArea.style.display = 'none';
+		team2ResultsArea.style.display = 'none';
+		result.innerHTML = 'PUSH';
+	}
+
+	formFinished = true;
+	calculateHoleButton.style.display = 'none';
+	resetButton.style.display = 'block';
+});
+
+resetButton.addEventListener('click', function () {
+	resetButton.style.display = 'none';
+	result.innerHTML = '';
+	calculateHoleButton.style.display = 'block';
+	team2Area.style.backgroundColor = 'rgb(255, 255, 255)';
+	team1Area.style.backgroundColor = 'rgb(255, 255, 255)';
+	team1ResultsArea.style.display = 'none';
+	team2ResultsArea.style.display = 'none';
+	courseSelect.value = '0';
+	holeSelect.value = '0';
+	document.getElementById('t1p1').value = '';
+	document.getElementById('t1p2').value = '';
+	document.getElementById('t2p1').value = '';
+	document.getElementById('t2p2').value = '';
+	parDisplay.style.display = 'none';
+});
