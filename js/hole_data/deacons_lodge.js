726b5867d04563f57ea7a5ef5d2fdea41335a3c7
Jake <jgevenson@gmail.com>
2020-05-04 13:23:05 -0500

    Create deacons_lodge.js

---
+++ js/hole_data/deacons_lodge.js
@@ -0,0 +1,182 @@
+export const deaconHoleInfo = [
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 1,
+		"black": 444,
+		"blue": 415,
+		"white": 399,
+		"holePar": 4,
+		"holeHandicap": 3
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 2,
+		"black": 540,
+		"blue": 520,
+		"white": 462,
+		"holePar": 5,
+		"holeHandicap": 5
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 3,
+		"black": 188,
+		"blue": 164,
+		"white": 144,
+		"holePar": 3,
+		"holeHandicap": 13
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 4,
+		"black": 419,
+		"blue": 399,
+		"white": 354,
+		"holePar": 4,
+		"holeHandicap": 9
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 5,
+		"black": 433,
+		"blue": 415,
+		"white": 400,
+		"holePar": 4,
+		"holeHandicap": 7
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 6,
+		"black": 115,
+		"blue": 108,
+		"white": 91,
+		"holePar": 3,
+		"holeHandicap": 17
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 7,
+		"black": 432,
+		"blue": 412,
+		"white": 385,
+		"holePar": 4,
+		"holeHandicap": 1
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 8,
+		"black": 408,
+		"blue": 374,
+		"white": 326,
+		"holePar": 4,
+		"holeHandicap": 15
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 9,
+		"black": 543,
+		"blue": 533,
+		"white": 480,
+		"holePar": 5,
+		"holeHandicap": 11
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 10,
+		"black": 373,
+		"blue": 357,
+		"white": 329,
+		"holePar": 4,
+		"holeHandicap": 14
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 11,
+		"black": 196,
+		"blue": 183,
+		"white": 176,
+		"holePar": 3,
+		"holeHandicap": 12
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 12,
+		"black": 340,
+		"blue": 320,
+		"white": 295,
+		"holePar": 4,
+		"holeHandicap": 18
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 13,
+		"black": 450,
+		"blue": 422,
+		"white": 380,
+		"holePar": 4,
+		"holeHandicap": 4
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 14,
+		"black": 534,
+		"blue": 512,
+		"white": 475,
+		"holePar": 5,
+		"holeHandicap": 8
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 15,
+		"black": 452,
+		"blue": 427,
+		"white": 413,
+		"holePar": 4,
+		"holeHandicap": 2
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 16,
+		"black": 375,
+		"blue": 362,
+		"white": 343,
+		"holePar": 4,
+		"holeHandicap": 16
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 17,
+		"black": 200,
+		"blue": 174,
+		"white": 144,
+		"holePar": 3,
+		"holeHandicap": 6
+	},
+	{
+		"courseString": "Deacon\'s Lodge",
+		"courseID": 0,
+		"hole": 18,
+		"black": 535,
+		"blue": 525,
+		"white": 495,
+		"holePar": 5,
+		"holeHandicap": 10
+	}
+];
