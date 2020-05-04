fc78b00a14424d673e952d004743e2b4a198c549
Jake <jgevenson@gmail.com>
2020-05-04 13:23:22 -0500

    Add files via upload

---
+++ js/hole_data/traditional.js
@@ -0,0 +1,182 @@
+export const traditionalHoleInfo = [
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 1,
+		"black": null,
+		"blue": null,
+		"white": 307,
+		"holePar": 4,
+		"holeHandicap": 9
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 2,
+		"black": null,
+		"blue": null,
+		"white": 145,
+		"holePar": 3,
+		"holeHandicap": 15
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 3,
+		"black": null,
+		"blue": null,
+		"white": 241,
+		"holePar": 4,
+		"holeHandicap": 17
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 4,
+		"black": null,
+		"blue": null,
+		"white": 275,
+		"holePar": 4,
+		"holeHandicap": 13
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 5,
+		"black": null,
+		"blue": null,
+		"white": 367,
+		"holePar": 4,
+		"holeHandicap": 1
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 6,
+		"black": null,
+		"blue": null,
+		"white": 156,
+		"holePar": 3,
+		"holeHandicap": 11
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 7,
+		"black": null,
+		"blue": null,
+		"white": 354,
+		"holePar": 4,
+		"holeHandicap": 3
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 8,
+		"black": null,
+		"blue": null,
+		"white": 318,
+		"holePar": 4,
+		"holeHandicap": 7
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 9,
+		"black": null,
+		"blue": null,
+		"white": 370,
+		"holePar": 4,
+		"holeHandicap": 5
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 10,
+		"black": null,
+		"blue": null,
+		"white": 346,
+		"holePar": 4,
+		"holeHandicap": 12
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 11,
+		"black": null,
+		"blue": null,
+		"white": 182,
+		"holePar": 3,
+		"holeHandicap": 18
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 12,
+		"black": null,
+		"blue": null,
+		"white": 305,
+		"holePar": 4,
+		"holeHandicap": 10
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 13,
+		"black": null,
+		"blue": null,
+		"white": 171,
+		"holePar": 3,
+		"holeHandicap": 16
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 14,
+		"black": null,
+		"blue": null,
+		"white": 292,
+		"holePar": 4,
+		"holeHandicap": 14
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 15,
+		"black": null,
+		"blue": null,
+		"white": 330,
+		"holePar": 4,
+		"holeHandicap": 4
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 16,
+		"black": null,
+		"blue": null,
+		"white": 318,
+		"holePar": 4,
+		"holeHandicap": 2
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 17,
+		"black": null,
+		"blue": null,
+		"white": 350,
+		"holePar": 4,
+		"holeHandicap": 8
+	},
+	{
+		"courseString": "Traditional",
+		"courseID": 1,
+		"hole": 18,
+		"black": null,
+		"blue": null,
+		"white": 365,
+		"holePar": 4,
+		"holeHandicap": 6
+	}
+];
---
+++ js/hole_data/whitebirch.js
@@ -0,0 +1,182 @@
+export const whitebirchHoleInfo = [
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 1,
+		"black": 341,
+		"blue": 322,
+		"white": 309,
+		"holePar": 4,
+		"holeHandicap": 13
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 2,
+		"black": 300,
+		"blue": 269,
+		"white": 244,
+		"holePar": 4,
+		"holeHandicap": 17
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 3,
+		"black": 442,
+		"blue": 423,
+		"white": 400,
+		"holePar": 4,
+		"holeHandicap": 3
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 4,
+		"black": 520,
+		"blue": 510,
+		"white": 490,
+		"holePar": 5,
+		"holeHandicap": 7
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 5,
+		"black": 176,
+		"blue": 168,
+		"white": 147,
+		"holePar": 3,
+		"holeHandicap": 15
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 6,
+		"black": 380,
+		"blue": 349,
+		"white": 329,
+		"holePar": 4,
+		"holeHandicap": 5
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 7,
+		"black": 536,
+		"blue": 483,
+		"white": 457,
+		"holePar": 5,
+		"holeHandicap": 1
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 8,
+		"black": 314,
+		"blue": 308,
+		"white": 293,
+		"holePar": 4,
+		"holeHandicap": 11
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 9,
+		"black": 217,
+		"blue": 207,
+		"white": 193,
+		"holePar": 3,
+		"holeHandicap": 9
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 10,
+		"black": 406,
+		"blue": 388,
+		"white": 370,
+		"holePar": 4,
+		"holeHandicap": 4
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 11,
+		"black": 479,
+		"blue": 433,
+		"white": 400,
+		"holePar": 4,
+		"holeHandicap": 2
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 12,
+		"black": 190,
+		"blue": 185,
+		"white": 144,
+		"holePar": 3,
+		"holeHandicap": 18
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 13,
+		"black": 373,
+		"blue": 363,
+		"white": 353,
+		"holePar": 4,
+		"holeHandicap": 14
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 14,
+		"black": 374,
+		"blue": 353,
+		"white": 326,
+		"holePar": 4,
+		"holeHandicap": 10
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 15,
+		"black": 502,
+		"blue": 461,
+		"white": 429,
+		"holePar": 5,
+		"holeHandicap": 6
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 16,
+		"black": 362,
+		"blue": 354,
+		"white": 324,
+		"holePar": 4,
+		"holeHandicap": 12
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 17,
+		"black": 205,
+		"blue": 193,
+		"white": 144,
+		"holePar": 3,
+		"holeHandicap": 16
+	},
+	{
+		"courseString": "Whitebirtch",
+		"courseID": 2,
+		"hole": 18,
+		"black": 563,
+		"blue": 523,
+		"white": 464,
+		"holePar": 5,
+		"holeHandicap": 8
+	}
+];
